aefa5f59a5e06715e2cfa87e76e8817f04de207b
Mayank <mayanksharmakv@gmail.com>
2023-07-14 19:51:28 +0530

    update js and ui

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 - A simple calculator built with HTML, CSS and JavaSript.
 - More features will be added soon.
   
-![image](https://github.com/Mayank-Sharma17/Simple-Calculator/assets/113251342/d530d9f5-37b8-45ff-861b-6e1a439dbc1c)
+![image](https://github.com/Mayank-Sharma17/Simple-Calculator/assets/113251342/736bd24a-4174-4d2b-9f9a-143afa40ea64)
--- script.js
+++ script.js
@@ -4,64 +4,51 @@ let display = document.querySelector("input");
 const buttons = document.querySelectorAll(".button");
 
 for (let button of buttons) {
+  button.addEventListener("click", (e) => {
+    // adding event listeners for each button
 
-  button.addEventListener("click", (e) => { // adding event listeners for each button 
-    
-    let buttonValue = e.target.innerHTML; 
-    
-    if(buttonValue == "C") {
+    let buttonValue = e.target.innerHTML;
 
+    if (buttonValue === "C") {
       result = "";
-      display.value = result;
-
-    } else if(buttonValue == "&lt;") {
-
+    } else if (buttonValue === "&lt;") {
       result = result.slice(0, -1); // remove the last char in the result string
-      display.value = result;
-      
-    } else if(buttonValue == "()") { // logic for '(' & ')'
+    } else if (buttonValue === "()") {
+      // logic for '(' & ')'
 
-      if(
-        result.indexOf("(") == -1 || 
-        result.indexOf("(") != -1 && 
-        result.indexOf(")") != -1 &&
-        result.lastIndexOf("(") < result.lastIndexOf(")")) {
-          buttonValue = "("
-          result += buttonValue;
-      } else if(
-        result.indexOf("(") != -1 &&
-        result.indexOf(")") == -1 ||
-        result.indexOf("(") != -1 &&
-        result.indexOf(")") != -1 &&
-        result.lastIndexOf("(") > result.lastIndexOf(")")) {
-          buttonValue = ")"
-          result += buttonValue;
+      if (
+        result.indexOf("(") === -1 ||
+        (result.indexOf("(") != -1 &&
+          result.indexOf(")") != -1 &&
+          result.lastIndexOf("(") < result.lastIndexOf(")"))
+      ) {
+        buttonValue = "(";
+      } else if (
+        (result.indexOf("(") != -1 && result.indexOf(")") === -1) ||
+        (result.indexOf("(") != -1 &&
+          result.indexOf(")") != -1 &&
+          result.lastIndexOf("(") > result.lastIndexOf(")"))
+      ) {
+        buttonValue = ")";
       }
-      display.value = result;
-      
-    } else if(buttonValue == "x") {
-
-      buttonValue = "*";
-
-      let currentResult = display.value;
-      result = currentResult + buttonValue;
-
-      display.value = result;
-      result = eval(result);
-      display.value = result;
-
-    } else if(buttonValue == "=") {
-
-      result = eval(result); // automatically evaluates js code
-      display.value = result;
-
-    } else {
-
       result += buttonValue;
-      display.value = result;
+    } else if (buttonValue === "x") {
+      buttonValue = "*";
+      let currentResult = display.value;
 
+      result = currentResult + buttonValue;
+    } else if (buttonValue === "%") {
+      result = result / 100;
+    } else if (buttonValue === "=") {
+      try {
+        result = math.evaluate(result);
+      } catch (error) {
+        result = "Invalid expression";
+      }
+    } else {
+      result += buttonValue;
     }
 
-  })
-  
+    display.value = result;
+  });
 }
