bccd6648445de4602b71cf93027a4db350f8ebf7
Mayank <mayanksharmakv@gmail.com>
2023-07-16 17:00:01 +0530

    update ui

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 - A simple calculator built with HTML, CSS and JavaSript.
 - More features will be added soon.
   
-![image](https://github.com/Mayank-Sharma17/Simple-Calculator/assets/113251342/736bd24a-4174-4d2b-9f9a-143afa40ea64)
+![image](https://github.com/Mayank-Sharma17/Simple-Calculator/assets/113251342/8d92abe1-96a4-45b8-813c-6e4d9277b9cf)
--- script.js
+++ script.js
@@ -3,52 +3,97 @@ let display = document.querySelector("input");
 
 const buttons = document.querySelectorAll(".button");
 
-for (let button of buttons) {
-  button.addEventListener("click", (e) => {
-    // adding event listeners for each button
+// Function to handle overall functionality
+const handleButtonClick = (buttonValue) => {
+  if (buttonValue === "C") {
+    result = "";
+  } else if (buttonValue === "&lt;") {
+    result = result.slice(0, -1); // remove the last char in the result string
+  } else if (buttonValue === "()") {
+    // logic for '(' & ')'
 
-    let buttonValue = e.target.innerHTML;
+    if (
+      result.indexOf("(") === -1 ||
+      (result.indexOf("(") != -1 &&
+        result.indexOf(")") != -1 &&
+        result.lastIndexOf("(") < result.lastIndexOf(")"))
+    ) {
+      buttonValue = "(";
+    } else if (
+      (result.indexOf("(") != -1 && result.indexOf(")") === -1) ||
+      (result.indexOf("(") != -1 &&
+        result.indexOf(")") != -1 &&
+        result.lastIndexOf("(") > result.lastIndexOf(")"))
+    ) {
+      buttonValue = ")";
+    }
+    result += buttonValue;
+  } else if (buttonValue === "x") {
+    buttonValue = "*";
+    let currentResult = display.value;
 
-    if (buttonValue === "C") {
-      result = "";
-    } else if (buttonValue === "&lt;") {
-      result = result.slice(0, -1); // remove the last char in the result string
-    } else if (buttonValue === "()") {
-      // logic for '(' & ')'
-
-      if (
-        result.indexOf("(") === -1 ||
-        (result.indexOf("(") != -1 &&
-          result.indexOf(")") != -1 &&
-          result.lastIndexOf("(") < result.lastIndexOf(")"))
-      ) {
-        buttonValue = "(";
-      } else if (
-        (result.indexOf("(") != -1 && result.indexOf(")") === -1) ||
-        (result.indexOf("(") != -1 &&
-          result.indexOf(")") != -1 &&
-          result.lastIndexOf("(") > result.lastIndexOf(")"))
-      ) {
-        buttonValue = ")";
-      }
-      result += buttonValue;
-    } else if (buttonValue === "x") {
-      buttonValue = "*";
-      let currentResult = display.value;
-
-      result = currentResult + buttonValue;
-    } else if (buttonValue === "%") {
-      result = result / 100;
-    } else if (buttonValue === "=") {
-      try {
-        result = math.evaluate(result);
-      } catch (error) {
-        result = "Invalid expression";
-      }
+    result = currentResult + buttonValue;
+  } else if (buttonValue === ".") {
+    if (display.value.includes(".")) {
+      // if '.' already present in result string
+      return;
     } else {
       result += buttonValue;
     }
+  } else if (buttonValue === "=") {
+    try {
+      result = math.evaluate(result);
+    } catch (error) {
+      result = "Invalid expression";
+    }
+  } else {
+    result += buttonValue;
+  }
 
-    display.value = result;
+  display.value = result;
+};
+
+// Add click event listeners to the buttons
+for (let button of buttons) {
+  button.addEventListener("click", (e) => {
+    let buttonValue = e.target.innerHTML;
+    handleButtonClick(buttonValue);
   });
 }
+
+// handle key press event
+const handleKeyDown = (event) => {
+  let key = event.key;
+  let buttonValue = "";
+
+  if (key === "Escape") {
+    buttonValue = "C";
+  } else if (key === "Backspace") {
+    buttonValue = "&lt;";
+  } else if (key === "b") {
+    buttonValue = "()";
+  } else if (key === "Enter") {
+    buttonValue = "=";
+  } else {
+    buttonValue = key;
+  }
+
+  // Find the button element with matching innerHTML
+  const matchedButton = Array.from(buttons).find(
+    (button) => button.innerHTML === buttonValue
+  );
+
+  if (matchedButton) {
+    matchedButton.classList.add("active");
+
+    // Remove the active class after a short delay
+    setTimeout(() => {
+      matchedButton.classList.remove("active");
+    }, 200);
+
+    handleButtonClick(buttonValue);
+  }
+};
+
+// Add keydown event listener to the document
+document.addEventListener("keydown", handleKeyDown);
